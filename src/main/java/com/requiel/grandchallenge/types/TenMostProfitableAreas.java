793541041b05271df9c77d5e62aa86513d44a6ae
Requiel20 <luca.battistelli@studenti.unicam.it>
2018-08-10 15:15:16 +0100

    ready to deploy for real

--- src/main/java/com/requiel/grandchallenge/types/TenMostProfitableAreas.java
+++
@@ -1,9 +0,0 @@
-package com.requiel.grandchallenge.types;
-
-import java.util.List;
-
-public class TenMostProfitableAreas {
-    public static TenMostProfitableAreas fromList(CellBasedTaxiTrip trip, List<CellBasedTaxiTrip> podium, double delay) {
-        return null;
-    }
-}
