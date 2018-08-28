5ba41b759c0b2c993412919580b230d727e6b3b8
Requiel20 <luca.battistelli@studenti.unicam.it>
2018-08-28 10:57:54 +0100

    adds redis source

--- src/main/java/com/requiel/grandchallenge/Solution.java
+++ src/main/java/com/requiel/grandchallenge/Solution.java
@@ -6,16 +6,25 @@ import com.requiel.grandchallenge.types.TaxiTrip;
 import com.requiel.grandchallenge.types.TenMostFrequentTrips;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
 
 import java.time.ZoneId;
 import java.util.ArrayList;
@@ -29,7 +38,7 @@ public class Solution {
 
     private static Logger log = LoggerFactory.getLogger(Solution.class);
 
-    private static String DEFAULT_INPUT_FILE = "grand-challenge-data.csv";
+    private static String DEFAULT_INPUT_FILE = "grand-challenge-data-1000000.csv";
     private static String DEFAULT_OUTPUT_FILE = "grand-challenge-output";
 
     public static void main(String[] args) throws Exception {
@@ -52,7 +61,8 @@ public class Solution {
 
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-        DataStream<String> inputData1 = env.readTextFile(input);
+        DataStream<String> inputData1 = env.addSource(new RedisCheckpointedSource("192.168.56.103"))
+                .setGeoLocationKey("location1");
 
         DataStream<CellBasedTaxiTrip> trips1 = inputData1
                 .flatMap(new Parser())
@@ -81,6 +91,58 @@ public class Solution {
             if (trip != null) {
                 collector.collect(trip);
             }
+        }
+    }
+
+    private static class RedisCheckpointedSource implements SourceFunction<String>, CheckpointedFunction {
+        //index to retrieve
+        private long count = 0L;
+
+        Jedis jedis;
+
+        private volatile boolean isRunning = true;
+
+        //keeps the state
+        private transient ListState<Long> checkpointedCount;
+
+        public RedisCheckpointedSource(String host) {
+            jedis = new Jedis(host, 6379);
+        }
+
+        @Override
+        public void snapshotState(FunctionSnapshotContext functionSnapshotContext) throws Exception {
+            this.checkpointedCount.clear();
+            this.checkpointedCount.add(count);
+        }
+
+        @Override
+        public void initializeState(FunctionInitializationContext context) throws Exception {
+            this.checkpointedCount = context
+                    .getOperatorStateStore()
+                    .getListState(new ListStateDescriptor<>("count", Long.class));
+
+            if (context.isRestored()) {
+                for (Long count : this.checkpointedCount.get()) {
+                    this.count = count;
+                }
+            }
+        }
+
+        @Override
+        public void run(SourceContext<String> context) throws Exception {
+            while (isRunning && count < 1000) {
+                // this synchronized block ensures that state checkpointing,
+                // internal state updates and emission of elements are an atomic operation
+                synchronized (context.getCheckpointLock()) {
+                    context.collect(jedis.get("" + count));
+                    count++;
+                }
+            }
+        }
+
+        @Override
+        public void cancel() {
+            isRunning = false;
         }
     }
 
